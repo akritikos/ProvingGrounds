ca8506565b6a053de486c4f63fd66e20398a6a8f
Alexandros Kritikos <akritikos@outlook.com>
2019-07-24 19:12:14 +0300

    Adds a basic project to simulate release

---
+++ src/ProvingGrounds.Runner/Program.cs
@@ -0,0 +1,13 @@
+namespace Kritikos.ProvingGrounds.Runner
+{
+	using System;
+
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.WriteLine($"Args used: {args}");
+		}
+	}
+}
