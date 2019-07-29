6797e1d1faea750cd59e6b0b32b6fdc8941e231f
Alexandros Kritikos <akritikos@outlook.com>
2019-07-29 15:47:58 +0300

    Adds a barebones password generator

---
+++ src/ProvingGrounds.Runner/PasswordOptions.cs
@@ -0,0 +1,15 @@
+namespace Kritikos.ProvingGrounds.Runner
+{
+	public class PasswordOptions
+	{
+		public int RequiredLength { get; set; } = 6;
+
+		public bool RequireNonAlphanumeric { get; set; } = false;
+
+		public bool RequireLowercase { get; set; } = true;
+
+		public bool RequireUppercase { get; set; } = false;
+
+		public bool RequireDigit { get; set; } = false;
+	}
+}
